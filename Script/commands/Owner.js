52fa4df3aa2014323fa736e4a5f61a6975979484
bbykhangit <bbykhan41@gmail.com>
2026-08-20 13:37:56 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -19,32 +19,32 @@ module.exports.run = async function ({ api, event }) {
 ╔═════════════════════ ✿
 ║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
 ╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+║ 👑 𝗡𝗮𝗺𝗲 : 𝗥丨𝗶𝗱𝗵 𝗜𝘀𝗹𝗮𝗺 𝗔𝘆𝗮𝗻
+║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗥丨𝗶𝗱𝗵
+║ 🎂 𝗔𝗴𝗲 : 𝟭𝟳+
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
+║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗝𝗼𝗯 𝗛𝗼𝗹𝗱𝗲𝗿𝘀
+║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗰𝗰
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗖𝗼𝘅 𝗕𝗮𝘇𝗮𝗿
 ╠═════════════════════ ✿
 ║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
 ╠═════════════════════ ✿
 ║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100044713412032
+║ fb.com/61591763713247
 ║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100044713412032
+║ m.me/61591763713247
 ║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
+║ wa.me/01825784274
 ║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
+║ t.me/@Ayan5478
 ╚═════════════════════ ✿
 `;
 
   const images = [
-    "https://i.imgur.com/gokzyKd.jpeg",
-    "https://i.imgur.com/g3hlQ0Z.jpeg",
-    "https://i.imgur.com/L7txp4M.jpeg",
-    "https://i.imgur.com/5dG8PS5.jpeg"
+    "https://i.postimg.cc/3RghXCnX/IMG-20260601-150213.jpg",
+    "https://i.postimg.cc/nrzmjzGB/ccb5cbf3248477f0a9f86b410307caea.jpg",
+    "https://i.postimg.cc/Dzn84025/Screenshot-2026-07-19-01-50-45-96-99c04817c0de5652397fc8b56c3b3817.jpg",
+    "https://i.postimg.cc/Bncv33x4/06d5711567ef90ee6c6125783c5f99ac-0.jpg"
   ];
 
   const randomImg = images[Math.floor(Math.random() * images.length)];
